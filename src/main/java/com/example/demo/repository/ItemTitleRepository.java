81cea91fea3a0e0dc3dfaf0091572da728bcfd81
sasaki-nina-6707 <420-student048@la-study.com>
2024-06-10 14:08:00 +0900

    検索結果と詳細機能の完成(仮)

--- src/main/java/com/example/demo/repository/ItemTitleRepository.java
+++ src/main/java/com/example/demo/repository/ItemTitleRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.ItemTitle;
 
 public interface ItemTitleRepository  extends JpaRepository<ItemTitle ,Integer>{
+
+	List<ItemTitle> findByNameContaining(String keyword);
 
 }
