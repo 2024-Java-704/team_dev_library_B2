a9387aa708a1bf03712d2699592ac4a9b6b8585d
ishii-wataru-7491 <420-student025@la-study.com>
2024-06-12 11:37:22 +0900

    usercontrollerfix history準備

--- src/main/java/com/example/demo/repository/RentalsRepository.java
+++ src/main/java/com/example/demo/repository/RentalsRepository.java
@@ -15,6 +15,12 @@ public interface RentalsRepository  extends JpaRepository<Rentals,Integer>{
 	List<Rentals> findByClosingDateBeforeAndStatusIs(LocalDate nowDate,Integer status);
 	//	List<Rentals> findByItemTitle(ItemTitle itemTitle);
 
-	List<Rentals> findByUserIdAndItemIdAndStatus(Integer uid, Integer iid, Integer status);
+	List<Rentals> findByUserIdAndItemIdAndStatusOrderByRentalDate(Integer uid, Integer iid, Integer status);
+	
+	List<Rentals> findByUserIdAndStatusAndClosingDateBeforeOrderByRentalDate(Integer userId,Integer status,LocalDate date);
+	List<Rentals> findByUserIdAndStatusOrderByRentalDate(Integer userId,Integer status);
+	
+	List<Rentals> findByUserIdOrderByRentalDate(Integer userId);
+	
 
 }
